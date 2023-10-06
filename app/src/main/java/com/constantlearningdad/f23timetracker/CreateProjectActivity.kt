50e57b823aec1553c45bfc2371ea2a1b9ac53584
JaretWright <Jaret.Wright@GeorgianCollege.ca>
2023-10-06 09:43:06 -0400

    Connected with Firebase and added a Project.kt model class

--- app/src/main/java/com/constantlearningdad/f23timetracker/CreateProjectActivity.kt
+++ app/src/main/java/com/constantlearningdad/f23timetracker/CreateProjectActivity.kt
@@ -2,10 +2,24 @@ package com.constantlearningdad.f23timetracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.constantlearningdad.f23timetracker.databinding.ActivityCreateProjectBinding
 
 class CreateProjectActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityCreateProjectBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_project)
+        binding = ActivityCreateProjectBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.createProjectButton.setOnClickListener {
+            if (binding.projectNameEditText.text.toString().isNotEmpty() &&
+                binding.descriptionEditText.text.toString().isNotEmpty())
+            {
+
+            }
+            else
+                Toast.makeText(this, "Both name & description must have values",Toast.LENGTH_LONG).show()
+        }
     }
 }
