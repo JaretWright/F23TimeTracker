d9366b9e1348d10c35ce456b5c5a7c03410637bb
JaretWright <Jaret.Wright@GeorgianCollege.ca>
2023-10-06 10:13:08 -0400

    Writing a project to the firestore database

--- app/src/main/java/com/constantlearningdad/f23timetracker/CreateProjectActivity.kt
+++ app/src/main/java/com/constantlearningdad/f23timetracker/CreateProjectActivity.kt
@@ -2,8 +2,10 @@ package com.constantlearningdad.f23timetracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.constantlearningdad.f23timetracker.databinding.ActivityCreateProjectBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 class CreateProjectActivity : AppCompatActivity() {
     private lateinit var binding : ActivityCreateProjectBinding
@@ -13,10 +15,27 @@ class CreateProjectActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.createProjectButton.setOnClickListener {
-            if (binding.projectNameEditText.text.toString().isNotEmpty() &&
-                binding.descriptionEditText.text.toString().isNotEmpty())
-            {
+            var projectName = binding.projectNameEditText.text.toString()
+            var description = binding.descriptionEditText.text.toString()
 
+            if (projectName.isNotEmpty() && description.isNotEmpty())
+            {
+                //create an instance of a Project
+                var project = Project(projectName, description)
+
+                //connect to Firebase-Firestore database
+                val db = FirebaseFirestore.getInstance().collection("projects")
+
+                //get a unique ID from Firestore
+                project.id = db.document().getId()
+
+                //save our new Project object to the DB using a unique ID
+                db.document().set(project)
+                    .addOnSuccessListener { Toast.makeText(this,"DB Updated", Toast.LENGTH_LONG).show() }
+                    .addOnFailureListener {exception ->
+                        Toast.makeText(this,"Error writing to DB",Toast.LENGTH_LONG).show()
+                        Log.w("DB_issue",exception.localizedMessage)
+                    }
             }
             else
                 Toast.makeText(this, "Both name & description must have values",Toast.LENGTH_LONG).show()
