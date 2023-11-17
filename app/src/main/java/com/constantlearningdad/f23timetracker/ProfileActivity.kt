d66de8116500dc81e1c8b1d46f75a2017bfebfb3
JaretWright <Jaret.Wright@GeorgianCollege.ca>
2023-11-17 09:39:42 -0500

    Week 10 - Added the profile activity

--- app/src/main/java/com/constantlearningdad/f23timetracker/ProfileActivity.kt
+++ app/src/main/java/com/constantlearningdad/f23timetracker/ProfileActivity.kt
@@ -2,10 +2,14 @@ package com.constantlearningdad.f23timetracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.constantlearningdad.f23timetracker.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
